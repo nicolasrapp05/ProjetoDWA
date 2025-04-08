0c20f49d41a53ee124457736b8667710def1958a
Nicolas <nicolasrapp05@gmail.com>
2025-04-08 20:37:37 -0300

    Criação do Projeto e da Model

---
+++ Models/Personagem.cs
@@ -0,0 +1,9 @@
+namespace ProjetoDWA.Models
+{
+    public class Personagem
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+    }
+}
