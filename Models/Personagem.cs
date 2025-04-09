a7ce45de78e8b66a5f13c2919cd906a720f81db3
Nicolas <nicolasrapp05@gmail.com>
2025-04-09 20:02:41 -0300

    Criação das regras dos campos da Model

--- Models/Personagem.cs
+++ Models/Personagem.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProjetoDWA.Models
 {
     public class Personagem
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório!")]
+        [MaxLength(50, ErrorMessage = "O máximo é de 50 caracteres")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório!")]
+        [MaxLength(50, ErrorMessage = "O máximo é de 50 caracteres")]
         public string Tipo { get; set; }
     }
 }
