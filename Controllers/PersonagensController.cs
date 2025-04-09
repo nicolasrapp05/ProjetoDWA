6d0f69ce571e5dbfc7e94daacf669fca5fd92e82
Nicolas <nicolasrapp05@gmail.com>
2025-04-09 12:30:41 -0300

    Fix

--- Controllers/PersonagensController.cs
+++ Controllers/PersonagensController.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProjetoDWA.Data;
 using ProjetoDWA.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProjetoDWA.Controllers
 {
